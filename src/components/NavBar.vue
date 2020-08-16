e117193e76e9276f688215143853ae0a3f779d00
d3vkk <donaldkk22@yahoo.com>
2020-08-16 16:02:43 +0300

    refactor(ui):  Change background color to green
    For NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="flex flex-row shadow-md p-2 justify-around">
+  <nav class="flex flex-row shadow-md p-2 justify-around bg-green-700 text-white">
     <router-link class="uppercase" to="/">One Health</router-link>
     <router-link to="/login">Log In</router-link>
     <router-link to="/signup">Sign Up</router-link>

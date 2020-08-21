e32dd1ea12536e0f53d7a370e0a40501b43d5d40
d3vkk <donaldkk22@yahoo.com>
2020-08-21 12:41:17 +0300

    refactor(ui): Remove Be on High Alert For
    From stats sidebar

--- src/components/StatsView.vue
+++ src/components/StatsView.vue
@@ -19,9 +19,6 @@
           <div class="flex items-center mt-4 py-2 px-6 text-white">
             <span class="mx-4" @click="toggleView(2)">Emerging Illnesses</span>
           </div>
-          <div class="flex items-center mt-4 py-2 px-6 text-white">
-            <span class="mx-4" @click="toggleView(3)">Be on High Alert For</span>
-          </div>
         </nav>
       </div>
       <div class="flex-1 flex flex-col">

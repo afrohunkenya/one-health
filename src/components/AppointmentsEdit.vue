f2bad3431cd8f2c577e51e407d57f9cf4026fb9c
d3vkk <donaldkk22@yahoo.com>
2020-08-24 16:13:01 +0300

    feat: Edit appointments for environmental health
    As a doctor

--- src/components/AppointmentsEdit.vue
+++ src/components/AppointmentsEdit.vue
@@ -2,7 +2,7 @@
   <div class="container max-w-xs p-8">
     <div class="text-2xl">Edit Appointment</div>
     <form @submit.prevent="editAppointment">
-      <div class="mb-4 mt-4 p-2">
+      <div class="mb-4 mt-4 p-2" v-if="healthType != 'E'">
         <div class="mb-2" v-if="healthType == 'H'">Patient First & Last Names</div>
         <div class="mb-2" v-if="healthType == 'A'">Owner's First & Last Names</div>
         <input
@@ -19,28 +19,19 @@
         <div class="mb-2 text-gray-600 text-sm">Use commas to separate each</div>
         <input type="text" maxlength="40" autocomplete required v-model="illness" />
       </div>
-      <div v-if="healthType == 'A'">
-        <div class="mb-4 mt-4 p-2">
-          <div class="mb-2">No. Of Animals</div>
-          <input type="number" min="1" autocomplete required v-model="animalCount" />
-        </div>
-        <div class="mb-4 mt-4 p-2">
-          <div class="mb-2">Location</div>
-          <input
-            type="text"
-            minlength="2"
-            maxlength="30"
-            autocomplete
-            required
-            v-model="animalLocation"
-          />
-        </div>
+      <div class="mb-4 mt-4 p-2" v-if="healthType == 'A'">
+        <div class="mb-2">No. Of Animals</div>
+        <input type="number" min="1" autocomplete required v-model="animalCount" />
+      </div>
+      <div class="mb-4 mt-4 p-2" v-if="healthType != 'H'">
+        <div class="mb-2">Location</div>
+        <input type="text" minlength="2" maxlength="30" autocomplete required v-model="location" />
       </div>
       <div class="mb-4 mt-4 p-2">
         <div class="mb-2">Date</div>
         <input class="bg-white" type="date" autocomplete required v-model="appointmentDate" />
       </div>
-      <div class="mb-4 mt-4 p-2">
+      <div class="mb-4 mt-4 p-2" v-if="healthType != 'E'">
         <div class="mb-2">Time</div>
         <input class="bg-white" type="time" autocomplete required v-model="appointmentTime" />
       </div>
@@ -55,17 +46,13 @@
         <div class="mb-2">Zoom Password</div>
         <input type="text" minlength="2" autocomplete required v-model="linkPassword" />
       </div>
+      <div class="mb-4 mt-4 p-2" v-if="healthType == 'E'">
+        <div class="mb-2">Intervention</div>
+        <textarea cols="30" rows="10" maxlength="500" autocomplete required v-model="intervention"></textarea>
+      </div>
       <div class="mb-4 mt-4 p-2">
         <div class="mb-2">Doctor Notes</div>
-        <textarea
-          name="notes"
-          cols="30"
-          rows="10"
-          maxlength="500"
-          autocomplete
-          required
-          v-model="notes"
-        ></textarea>
+        <textarea cols="30" rows="10" maxlength="500" autocomplete required v-model="notes"></textarea>
       </div>
       <div class="flex flex-row">
         <button
@@ -98,7 +85,8 @@ export default {
       patientNamesArr: null,
       healthType: null,
       animalCount: null,
-      animalLocation: null,
+      location: null,
+      intervention: null,
     };
   },
   methods: {
@@ -128,43 +116,63 @@ export default {
       }:00 GMT`;
     },
     editAppointment() {
-      const formattedTime = Date.parse(this.formatTime()) / 100;
-      if (this.healthType == "H") {
-        //If Human Health
+      if (this.healthType == "E") {
+        //If Environmental Health
         db.collection("appointments")
           .doc(this.savedEditAppointment.id)
           .set({
             doctorId: this.savedEditAppointment.doctorId,
-            patientId: this.savedEditAppointment.patientId,
             illness: this.illness.toLowerCase().split(","),
-            time: formattedTime,
+            location: this.location,
+            date: this.appointmentDate,
             linkURL: this.linkURL,
             linkPassword: this.linkPassword,
+            intervention: this.intervention.trim(),
             notes: this.notes.trim(),
           })
           .then(function () {
             this.$router.push("/dashboard");
           })
           .catch((err) => console.log(err));
-      } else if (this.healthType == "A") {
-        //If Animal Health
-        db.collection("appointments")
-          .doc(this.savedEditAppointment.id)
-          .set({
-            doctorId: this.savedEditAppointment.doctorId,
-            patientId: this.savedEditAppointment.patientId,
-            illness: this.illness.toLowerCase().split(","),
-            animalCount: this.animalCount,
-            animalLocation: this.animalLocation,
-            time: formattedTime,
-            linkURL: this.linkURL,
-            linkPassword: this.linkPassword,
-            notes: this.notes.trim(),
-          })
-          .then(function () {
-            this.$router.push("/dashboard");
-          })
-          .catch((err) => console.log(err));
+      } else {
+        const formattedTime = Date.parse(this.formatTime()) / 100;
+        if (this.healthType == "H") {
+          //If Human Health
+          db.collection("appointments")
+            .doc(this.savedEditAppointment.id)
+            .set({
+              doctorId: this.savedEditAppointment.doctorId,
+              patientId: this.savedEditAppointment.patientId,
+              illness: this.illness.toLowerCase().split(","),
+              time: formattedTime,
+              linkURL: this.linkURL,
+              linkPassword: this.linkPassword,
+              notes: this.notes.trim(),
+            })
+            .then(function () {
+              this.$router.push("/dashboard");
+            })
+            .catch((err) => console.log(err));
+        } else if (this.healthType == "A") {
+          //If Animal Health
+          db.collection("appointments")
+            .doc(this.savedEditAppointment.id)
+            .set({
+              doctorId: this.savedEditAppointment.doctorId,
+              patientId: this.savedEditAppointment.patientId,
+              illness: this.illness.toLowerCase().split(","),
+              animalCount: this.animalCount,
+              environmentalLocation: this.location,
+              time: formattedTime,
+              linkURL: this.linkURL,
+              linkPassword: this.linkPassword,
+              notes: this.notes.trim(),
+            })
+            .then(function () {
+              this.$router.push("/dashboard");
+            })
+            .catch((err) => console.log(err));
+        }
       }
     },
   },
@@ -173,23 +181,26 @@ export default {
     this.savedEditAppointment = localStorageData
       ? JSON.parse(localStorageData).savedEditAppointment
       : {};
-    this.patientNames =
-      this.savedEditAppointment.patient.firstName +
-      " " +
-      this.savedEditAppointment.patient.lastName;
-    this.illness = this.savedEditAppointment.illness.join(",");
-    this.linkURL = this.savedEditAppointment.linkURL;
-    this.linkPassword = this.savedEditAppointment.linkPassword;
-    this.notes = this.savedEditAppointment.notes;
-
     var localStorageHealthType = localStorage.getItem("healthType");
     this.healthType = localStorageHealthType
       ? JSON.parse(localStorageHealthType).healthType
       : "";
-    if (this.healthType == "A") {
+    if (this.healthType != "E") {
+      this.patientNames =
+        this.savedEditAppointment.patient.firstName +
+        " " +
+        this.savedEditAppointment.patient.lastName;
+    } else if (this.healthType == "E") {
+      this.location = this.savedEditAppointment.environmentLocation;
+      this.intervention = this.savedEditAppointment.intervention;
+    } else if (this.healthType == "A") {
       this.animalCount = this.savedEditAppointment.animalCount;
-      this.animalLocation = this.savedEditAppointment.animalLocation;
+      this.location = this.savedEditAppointment.animalLocation;
     }
+    this.illness = this.savedEditAppointment.illness.join(",");
+    this.linkURL = this.savedEditAppointment.linkURL;
+    this.linkPassword = this.savedEditAppointment.linkPassword;
+    this.notes = this.savedEditAppointment.notes;
   },
 };
 </script>

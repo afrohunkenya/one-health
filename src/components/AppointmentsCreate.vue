fcc8b26ffab40aab9387655ce2f22d3be1db565d
d3vkk <donaldkk22@yahoo.com>
2020-08-30 13:50:14 +0300

    feat: Update and create stats
    When creating appointments

--- src/components/AppointmentsCreate.vue
+++ src/components/AppointmentsCreate.vue
@@ -2,7 +2,7 @@
   <div class="container max-w-xs p-8">
     <div class="text-2xl" v-if="healthType != 'E'">Create Appointment</div>
     <div class="text-2xl" v-if="healthType == 'E'">Create Inspection</div>
-    <form @submit.prevent="sendEmail">
+    <form @submit.prevent="updateStats">
       <div class="mb-4 mt-4 p-2" v-if="healthType != 'E'">
         <div class="mb-2" v-if="healthType == 'H'">Patient First & Last Names</div>
         <div class="mb-2" v-if="healthType == 'A'">Owner's First & Last Names</div>
@@ -73,6 +73,7 @@
 </template>
 
 <script>
+import firebase from "firebase/app";
 import db from "./firebaseInit";
 
 export default {
@@ -92,9 +93,162 @@ export default {
       animalCount: null,
       location: null,
       intervention: null,
+      statsMonth: null,
+      statsWeekOfMonth: null,
+      storedStats: [],
+      newStats: [],
+      updatedStats: [],
+      addedStats: [],
+      unUpdatedStats: [],
+      allStats: [],
     };
   },
   methods: {
+    updateStats() {
+      this.illnessConfirmed = true;
+      if (this.illnessConfirmed) {
+        this.illness = "typhoid";
+        this.appointmentDate = "2020-07-21";
+        this.newStats = this.illness.split(",");
+
+        Date.prototype.getWeekOfMonth = function () {
+          var firstWeekday = new Date(
+            this.getFullYear(),
+            this.getMonth(),
+            1
+          ).getDay();
+          var offsetDate = this.getDate() + firstWeekday - 1;
+          return Math.floor(offsetDate / 7);
+        };
+
+        //parse ISO As Local
+        var dateArr = this.appointmentDate.split("-");
+
+        // Get Month & Week of Month
+        this.statsMonth = parseInt(dateArr[1]).toString();
+        var parsedDate = new Date(dateArr[0], dateArr[1] - 1, dateArr[2]);
+        this.statsWeekOfMonth = parsedDate.getWeekOfMonth();
+
+        // Get stored stats
+        db.collection("stats2020")
+          .where(
+            firebase.firestore.FieldPath.documentId(),
+            "==",
+            this.statsMonth
+          )
+          .get()
+          .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+              const data = {
+                content: doc.data().content,
+              };
+              this.storedStats = data.content;
+
+              //Update existing stats
+              for (let j = 0; j < this.newStats.length; j++) {
+                const newStat = this.newStats[j];
+                for (let k = 0; k < this.storedStats.length; k++) {
+                  const storedStat = this.storedStats[k];
+                  if (newStat == storedStat.illness) {
+                    var updatedCases = [];
+                    for (let l = 0; l < storedStat.cases.length; l++) {
+                      l == this.statsWeekOfMonth - 1
+                        ? updatedCases.push(storedStat.cases[l] + 1)
+                        : updatedCases.push(storedStat.cases[l]);
+                    }
+                    this.updatedStats.push({
+                      illness: storedStat.illness,
+                      cases: updatedCases,
+                    });
+                  }
+                }
+              }
+
+              //Add bool to check if newStat has been added already
+              //i.e. it wasn't updated earlier
+              var newStatsTemp = [];
+              for (let n = 0; n < this.newStats.length; n++) {
+                const newStat = this.newStats[n];
+                newStatsTemp.push({
+                  illness: newStat,
+                  isAdded: false,
+                });
+              }
+
+              //Add non-existant stats
+              const updatedStatsLength = this.updatedStats.length;
+              for (let j = 0; j < newStatsTemp.length; j++) {
+                const newStat = newStatsTemp[j];
+                for (let k = 0; k < updatedStatsLength; k++) {
+                  const updatedStat = this.updatedStats[k];
+                  if (newStat.illness == updatedStat.illness) {
+                    newStat.isAdded = true;
+                  } else if (k == updatedStatsLength - 1 && !newStat.isAdded) {
+                    var addedCases = [];
+                    for (let l = 0; l < 4; l++) {
+                      l == this.statsWeekOfMonth - 1
+                        ? addedCases.push(1)
+                        : addedCases.push(0);
+                    }
+                    this.addedStats.push({
+                      illness: newStat.illness,
+                      cases: addedCases,
+                    });
+                  }
+                }
+              }
+
+              //Add bool to check if storedStat has been filtered already
+              //i.e. it was found already
+              var storedStatsTemp = [];
+              for (let n = 0; n < this.storedStats.length; n++) {
+                const storedStat = this.storedStats[n];
+                storedStatsTemp.push({
+                  illness: storedStat.illness,
+                  cases: storedStat.cases,
+                  isFiltered: false,
+                });
+              }
+
+              //Keep unupdated stats
+              for (let k = 0; k < storedStatsTemp.length; k++) {
+                const storedStat = storedStatsTemp[k];
+                for (let j = 0; j < this.updatedStats.length; j++) {
+                  const updatedStat = this.updatedStats[j];
+                  if (updatedStat.illness == storedStat.illness) {
+                    storedStat.isFiltered = true;
+                  } else if (
+                    j == this.updatedStats.length - 1 &&
+                    !storedStat.isFiltered
+                  ) {
+                    this.unUpdatedStats.push({
+                      cases: storedStat.cases,
+                      illness: storedStat.illness,
+                    });
+                  }
+                }
+              }
+
+              //Combine all stats
+              this.allStats = this.updatedStats.concat(
+                this.unUpdatedStats,
+                this.addedStats
+              );
+
+              db.collection("stats2020")
+                .doc(this.statsMonth)
+                .set({
+                  content: this.allStats,
+                })
+                .then(function () {})
+                .catch((err) => console.log(err));
+              this.sendEmail();
+            });
+          });
+      } else {
+        this.sendEmail();
+      }
+    },
     sendEmail() {
       var userEmail = localStorage.getItem("userEmail");
       if (userEmail) {
